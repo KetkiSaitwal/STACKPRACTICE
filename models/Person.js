3962b98de4810e737153a63256ffc41822dfc616
KetkiSaitwal <48442551+KetkiSaitwal@users.noreply.github.com>
2020-09-24 17:47:19 +0530

    Update Person.js

--- models/Person.js
+++ models/Person.js
@@ -1,1 +1,31 @@
 
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PersonSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    username: {
+        type: String,
+    },
+    profilepic: {
+        type: String,
+        default: ".\Child.png", //default image
+    },
+    date: {
+        type: Date,
+        default: Date.now(),
+    }
+});
+
+module.exports = Person = mongoose.model("testnode", PersonSchema)
